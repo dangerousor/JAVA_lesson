8c43ea61c3e1deb4791290006cd89b162d2cafcf
dangerousor <1192350292@qq.com>
2018-03-15 17:21:13 +0800

    lesson1

---
+++ src/Bubble.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Bubble {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
